b38975360bac64b0ecbd8d3d42ed5418db9a7cdb
Jaco Briers <jbriers@imqs.co.za>
2015-07-27 08:44:20 +0200

    Add /records API

--- server/main.go
+++ server/main.go
@@ -2,14 +2,19 @@ package main
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 	"log"
+	"math/rand"
 	"net/http"
+	"strconv"
 )
 
-const recordCount = 10000
+const recordCount = 1000000
+const columnCount = 11
 
-var columns = [...]string{"ID", "A", "B", "C", "D", "E", "F", "G", "H", "I"}
+var columns = [columnCount]string{"ID", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J"}
+var letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
 
 func main() {
 
@@ -23,17 +28,64 @@ func main() {
 	log.Fatal(http.ListenAndServe(":2050", nil))
 }
 
+func httpSendError(w http.ResponseWriter, err error) {
+	w.WriteHeader(http.StatusBadRequest)
+	fmt.Fprintf(w, "%v", err)
+}
+
 func handlerRecordCount(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "%v", recordCount)
 }
 
 func handlerColumns(w http.ResponseWriter, r *http.Request) {
-	jsonString, _ := json.Marshal(columns)
-	fmt.Fprintf(w, "%s", jsonString)
+	jsonString, err := json.Marshal(columns)
+	if err == nil {
+		fmt.Fprintf(w, "%s", jsonString)
+	} else {
+		httpSendError(w, err)
+	}
+}
+
+func randSeq(n int) string {
+	b := make([]rune, n)
+	for i := range b {
+		b[i] = letters[rand.Intn(len(letters))]
+	}
+	return string(b)
 }
 
 func handlerRecords(w http.ResponseWriter, r *http.Request) {
-	fromIdx := r.FormValue("from")
-	toIdx := r.FormValue("to")
-	fmt.Fprintf(w, "From %v to %v", fromIdx, toIdx)
+	fromIdx, err := strconv.Atoi(r.FormValue("from"))
+	if err != nil {
+		httpSendError(w, err)
+		return
+	}
+	toIdx, err := strconv.Atoi(r.FormValue("to"))
+	if err != nil {
+		httpSendError(w, err)
+		return
+	}
+	if toIdx < fromIdx || fromIdx < 0 || fromIdx > recordCount-1 || toIdx < 0 || toIdx > recordCount-1 {
+		httpSendError(w, errors.New("Invalid from or to parameter"))
+		return
+	}
+
+	records := make([][]string, toIdx-fromIdx)
+	for i := range records {
+		records[i] = make([]string, columnCount)
+		for j := range records[i] {
+			if j == 0 {
+				records[i][j] = fmt.Sprintf("%v", i)
+			} else {
+				records[i][j] = fmt.Sprintf("%s%v-%s", columns[j], i, randSeq(3))
+			}
+		}
+	}
+
+	jsonString, err := json.Marshal(records)
+	if err == nil {
+		fmt.Fprintf(w, "%s", jsonString)
+	} else {
+		httpSendError(w, err)
+	}
 }
