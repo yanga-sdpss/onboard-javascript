2a403002e4415b2256bd3db90715413d209d5bab
Yanga Ndamane <yanga@pop-os.localdomain>
2019-11-17 15:37:34 +0200

    Made few changes

--- app.ts
+++ app.ts
@@ -1,35 +1,91 @@
 // window.onload = () => { $("body").text("Hello world"); }
-async function getColumnNames() :Promise<string[]>{
-    const response = await fetch('http://localhost:2050/recordCount');
-    return await response.json();
-}
+let totalRecordCount;
+let columnNames: string[];
+let records: any[];
+let from: number = 1;
+let to: number =30;
 
-async function getRecords(fromID: number, toID: number): Promise<string[][]> {
-    const response = await fetch('http://localhost:2050/records?from=${(fromID)}&to=${(toID)}');
-    return await response.json();
-}
+function BuildTable () {
+    const body = document.getElementsByTagName("body")[0];
+    
+    const ourTable = document.createElement("table");
+    ourTable.setAttribute("id", "Table");
 
-async function getRecordCount() :Promise<number> {
-    const response = await fetch('http://localhost:2050/recordCount');
-    return await response.json();
-}
+    const thead = document.createElement("thead");
+    ourTable.appendChild(thead);
 
-async function placeRecords(fromID: number, toID: number): Promise<number[]> {
-    const records = await getRecords(fromID, toID)
-    let appendable = '';
-    for (const record of records) {
-        appendable += '<tr id="table-row-${record[0]}">';
-        for (const column of record) {
-            appendable += '<td align="center">${column}</td>';
+    for (let c = 0; c < columnNames.length; c++){
+        thead
+        .appendChild(document.createElement("th"))
+        .appendChild(document.createTextNode(columnNames[c]));
+    }
+
+    for (let i = 0; i < records.length; i++){
+        const tableRow = document.createElement("tr");
+        ourTable.appendChild(tableRow);
+
+        const innerArrayLength = records[i].length;
+
+        for (let j = 0; j < innerArrayLength; j++) {
+            tableRow
+            .appendChild(document.createElement("td"))
+            .appendChild(document.createTextNode(records[i][j]));
         }
-    appendable += '</tr>';
     }
-    $("#wrapper-table-content-body").empty();
-    $("#wrapper-table-content-body").append(appendable);
-    return [fromID, toID];
-    }
-async function placeRecordsFromCursor(cursor: number[]) : Promise<number[]> {
-    cursor = cursor.sort((a,b) => {return a-b});
-    return await placeRecords(cursor[0], cursor[1]);
+    body.appendChild(ourTable);
 }
+
+function getRecordCount() {
+    $.ajax({
+        url: "/recordCount",
+        type: "GET",
+        timeout: 1200,
+        dataType: "text"
+    })
+    .done(function(responseText: any){
+        totalRecordCount = responseText;
+    })
+    .fail(function(){
+        alert("Could not retrive total record number");
+    });
+}
+
+function GetColumnNames() {
+    $.ajax({
+        url: "/columns",
+        type: "GET",
+        timeout: 12000,
+        dataType: "json"
+    })
+    .done(function(responseJSON: any){
+        columnNames = responseJSON;
+        Sync();
+    })
+    .fail(function() {
+        alert("Could not display column names");
+    });
+}
+
+function getActualRecords() {
+    $.ajax({
+        url: "/records?from=" + from + "&to" + to,
+        timeout: 12000,
+        dataType: "json"
+    })
+    .done(function(responseRecords: any){
+        records = responseRecords;
+        nextSync();
+    })
+    .fail(function(){
+        alert("No available records for your selection")
+    });
+}
+
+
+function Sync() {
+    getActualRecords();
+}
+
+function nextSync() {
+    BuildTable();
 }
